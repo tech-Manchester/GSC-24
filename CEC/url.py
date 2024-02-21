6eac908b60606628abbad4cdd5d5242bc71bfbc4
Akshat  Mahajan <140583575+tech-Manchester@users.noreply.github.com>
2024-02-21 17:54:09 +0530

    Update url.py

--- CEC/url.py
+++ CEC/url.py
@@ -1,12 +1,16 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"),
-    path('login',views.login, name="login"),
-    path('signup',views.signup, name="signup"),
-    path('postsign/',views.postsign,name ="postsign"),
-    path('presign/',views.presign,name ="presign"),
-    path('calculator',views.calculator, name="calculator"),
+    path('', views.home, name="home"),
+    path('login', views.login, name="login"),
+    path('signup', views.signup, name="signup"),
+    path('postsign/', views.postsign, name="postsign"),
+    path('presign/', views.presign, name="presign"),
+    path('calculator', views.calculator, name="calculator"),
+    path('about', views.about, name="about"),
+    path('welcome/<str:firebase_uid>/', views.welcome, name='welcome'),
+    path('profile', views.profile, name="profile"),
+
+    # Add other URLs as needed
 ]
